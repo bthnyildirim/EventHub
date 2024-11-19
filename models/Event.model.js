89174061473778638c4fe3daded0d5020af8f56d
Batuhan Yildirim <bthn.yildirim@gmail.com>
2024-11-19 20:59:08 +0100

    add event model

--- models/Event.model.js
+++ models/Event.model.js
@@ -1,1 +1,27 @@
 const mongoose = require("mongoose");
+const { Schema, model } = require("mongoose");
+
+const EventSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, "Email is required."],
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required."],
+  },
+  dateTime: { type: Date, required: [true, "Date and time are required."] },
+  pricing: {
+    min: { type: Number, required: [true, "Minimum price is required."] },
+    max: { type: Number, required: [true, "Maximum Price is required."] },
+  },
+  map: { type: String, required: true },
+  image: { type: String, required: true },
+  organizer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: false,
+  },
+});
+
+module.exports = mongoose.model("Event", EventSchema);
