d5097c1f1cc93acfad0a6e2a4ba4e782e339ce47
Batuhan Yildirim <bthn.yildirim@gmail.com>
2024-11-26 17:28:28 +0100

    implement create function for organizer

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -60,21 +60,18 @@ router.post("/signup", (req, res, next) => {
       const hashedPassword = bcrypt.hashSync(password, salt);
 
       // Create the new user in the database
-      // We return a pending promise, which allows us to chain another `then`
       return User.create({ email, password: hashedPassword, name, userType });
     })
     .then((createdUser) => {
       // Deconstruct the newly created user object to omit the password
-      // We should never expose passwords publicly
-
       console.log(createdUser);
-      const { email, name, _id, UserType } = createdUser;
+      const { email, name, _id, userType } = createdUser;
 
       // Create a new object that doesn't expose the password
       const user = { email, name, _id, userType };
 
       // Send a json response containing the user object
-      res.status(201).json({ user: user });
+      res.status(201).json({ user });
     })
     .catch((err) => next(err)); // In this case, we send error handling to the error handling middleware.
 });
@@ -102,11 +99,11 @@ router.post("/login", (req, res, next) => {
       const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
 
       if (passwordCorrect) {
-        // Deconstruct the user object to omit the password
-        const { _id, email, name } = foundUser;
+        // Deconstruct the user object to include userType
+        const { _id, email, name, userType } = foundUser;
 
         // Create an object that will be set as the token payload
-        const payload = { _id, email, name };
+        const payload = { _id, email, name, userType };
 
         // Create a JSON Web Token and sign it
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
@@ -115,7 +112,7 @@ router.post("/login", (req, res, next) => {
         });
 
         // Send the token as the response
-        res.status(200).json({ authToken: authToken, redirectUrl: "/events" });
+        res.status(200).json({ authToken, redirectUrl: "/events" });
       } else {
         res.status(401).json({ message: "Unable to authenticate the user" });
       }
@@ -129,10 +126,11 @@ router.post("/login", (req, res, next) => {
 // GET  /auth/verify  -  Used to verify JWT stored on the client
 router.get("/verify", isAuthenticated, (req, res, next) => {
   try {
+    const { _id, email, name, userType } = req.payload; // Ensure userType is included
     console.log("Decoded Payload:", req.payload);
-    res.status(200).json(req.payload);
+    res.status(200).json({ _id, email, name, userType }); // Send back userType
   } catch (error) {
-    console.error("Error in /verify route:", error.message);
+    console.error("Error in /auth/verify route:", error.message);
     res.status(500).json({ message: "Token verification failed" });
   }
 });
