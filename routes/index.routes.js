d4d6e403adf1957fa3f21752b39ef80a0f56e996
Batuhan Yildirim <bthn.yildirim@gmail.com>
2024-11-28 11:54:19 +0100

    add healtcheck endpoint

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
+});
+
+// GET/API/HEALTH
+
+router.get("/health", (req, res) => {
+  mongoose.connection.db
+    .admin()
+    .ping()
+    .then(() => {
+      res.status(200).json({
+        status: "ok",
+        timestamp: new Date().toISOString(),
+      });
+    })
+    .catch((err) => {
+      console.error("MongoDB ping failed:", err);
+      res.status(500).json({
+        status: "error",
+        message: "Failed to connect MongoDB",
+      });
+    });
 });
 
 module.exports = router;
